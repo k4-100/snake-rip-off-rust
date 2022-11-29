42234e8f0b6fd2718da096e0aaa997ab46d17503
k4-100 <k4-100@protonmail.com>
2022-11-29 17:26:22 +0100

    Added basic text

--- src/plugins/player.rs
+++ src/plugins/player.rs
@@ -32,7 +32,7 @@ impl Plugin for PlayerPlugin{
 
 // region: --- PlayerPlugin systems
 
-fn setup(mut commands: Commands ){
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
 
 
     commands.spawn(Camera2dBundle{
@@ -152,6 +152,29 @@ fn setup(mut commands: Commands ){
           ..default()
         }
     )
+    );
+
+    commands.spawn(
+      (
+        TextBundle::from_section(
+          "Hello guys!", 
+          TextStyle { 
+            font: asset_server.load("Ubuntu-M.ttf"), 
+            font_size: 100., 
+            color: Color::WHITE 
+          }
+        )
+        .with_text_alignment(TextAlignment::TOP_CENTER)
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                bottom: Val::Px(5.0),
+                right: Val::Px(15.0),
+                ..default()
+            },
+            ..default()
+        }),
+      )
     );
 
 }
